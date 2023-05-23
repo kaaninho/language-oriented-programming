0ee29e96b2025f97c97c4c00c8662572b2d96bae
Kaan Sahin <kaan.sahin@active-group.de>
2023-05-23 15:29:49 +0200

    Change to comments

--- code/intro.rkt
+++ code/intro.rkt
@@ -3,6 +3,6 @@
 (define pi 3.141592)
 
 ;; READ und EVAL
-#;(read-syntax "basic" (open-input-string "(+ 1 2)"))
-#;#`(+ 1 2)
-#;(eval #`(+ 1 2))
+;; (read-syntax "intro" (open-input-string "(+ 1 2)"))
+;; #`(+ 1 2)
+;; (eval #`(+ 1 2))
