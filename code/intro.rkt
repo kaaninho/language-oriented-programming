35da96edd8afe727817ba83fea8a576d997fc6ec
Kaan Sahin <kaan.sahin@active-group.de>
2023-05-30 18:23:50 +0200

    Improve read eval code

--- code/intro.rkt
+++ code/intro.rkt
@@ -5,8 +5,10 @@
 ;; READ und EVAL
 ;; (read-syntax "intro" (open-input-string "(+ 1 2)"))
 ;; #`(+ 1 2)
+;; #`(+ 1 2 pi)
+;; #`(+ 1 2 #,pi)
 ;; (eval #`(+ 1 2))
 ;; (list + 1 2)
-;; (eval #`(2 + 1))
 ;; (list 2 + 1)
+;; (eval #`(2 + 1))
 ;; (list define abc "test")
