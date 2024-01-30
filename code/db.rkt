538a356c3f9a8c9d97aa5bcf95b5afd6c2925898
Kaan Sahin <kaan.sahin@active-group.de>
2024-01-30 11:16:25 +0100

    Add example to beginning

--- code/db.rkt
+++ code/db.rkt
@@ -34,7 +34,15 @@ PRINT "Summe"
 (define PUT #f)
 (define = #f)
 
-
+#;
+(db (SHOW-DB)
+    (PUT "milk" 1.50)
+    (PUT "water" 1.00)
+    (x = GET "water")
+    (PRINT x)
+    (SHOW-DB)
+    (y = GET "milk")
+    (PRINT "Summe"))
 
 
 
@@ -97,13 +105,4 @@ PRINT "Summe"
                                  [(var = GET name)
                                   #`(define var (get-it name))]))
                  (syntax->list #'(statement ...))))]))
-     
-#;
-(db (SHOW-DB)
-    (PUT "milk" 1.50)
-    (PUT "water" 1.00)
-    (x = GET "water")
-    (PRINT x)
-    (SHOW-DB)
-    (y = GET "milk")
-    (PRINT "Summe"))
+   
